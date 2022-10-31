769cca71e22e44d04042b907f9e3e6f5302c9ded
ADEYA DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2022-11-01 00:40:50 +0300

    Update sql_android.kt

--- sql_android.kt
+++ sql_android.kt
@@ -5,3 +5,12 @@ WHERE park_visitors < 1000000
 //SQL query to get the number of distinct cities in the park table
 SELECT COUNT(DISTINCT city) FROM park
 
+//SQL query to get the total number of visitors to parks in San Francisco.
+SELECT SUM(park_visitors) FROM park
+WHERE city = "San Francisco"
+
+//SQL query to the top 5 parks (names only) along with their visitor count that had the most visitors, in descending order.
+SELECT name, park_visitors FROM park
+ORDER BY park_visitors DESC
+LIMIT 5
+
